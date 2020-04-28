91894585b4904837f74a285d216e8fbbed57e20a
regdata <regdata.voice@gmail.com>
2020-04-27 21:17:58 -0400

    passing data with props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import { Home } from './components/Home';
 
 class App extends React.Component {
   render() {
+    let user = {
+      name: 'Anna',
+      hobbies: ['Sports', 'Reading'],
+    };
+
     return (
       <div className='container'>
         <div className='row'>
@@ -14,7 +19,7 @@ class App extends React.Component {
         </div>
         <div className='row'>
           <div className='col-xs-10 col-xs-offset-1'>
-            <Home />
+            <Home name={'Richard G'} age={37} user={user} />
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,25 @@ export class Home extends React.Component {
     return (
       <div>
         <p>In a new Component!</p>
+        <p>
+          Your name is {this.props.name}, your age is {this.props.age}
+        </p>
+        <p>User Object => Name: {this.props.user.name}</p>
+        <div>
+          <h4>Hobbies</h4>
+          <ul>
+            {this.props.user.hobbies.map((hobby, i) => (
+              <li key={i}>{hobby}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
 }
+
+Home.propTypes = {
+  name: React.PropTypes.string,
+  age: React.PropTypes.number,
+  user: React.PropTypes.object,
+};
